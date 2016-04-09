b0dc36ac861fa916bf997d42be448eb2f088ec4b
Ahdab Serhan <ms.ahdab@gmail.com>
2016-04-09 17:54:32 +0300

    lab1

--- Hw1/Hw1-languegesProgramming/Program.cs
+++ Hw1/Hw1-languegesProgramming/Program.cs
@@ -6,10 +6,279 @@ using System.Threading.Tasks;
 
 namespace Hw1_languegesProgramming
 {
-    class Program
+    class Number
+    {
+        private int n;
+        char[] digits;
+        
+
+        public Number(string digits, int n)
+        {
+
+            int i;
+            int powerOfTwo = 1;
+            
+            while (powerOfTwo < n && powerOfTwo < 2147483648)
+            {
+                powerOfTwo *= 2;
+            }
+           
+            for (i = 0; i < powerOfTwo; i++)
+               this. digits[i] = digits[i];
+
+        }
+        public Number()
+        {
+            Number num = new Number();
+            num.n = 0;
+            num.digits[0] = '0';
+           
+        }
+        public int N { get { return n; } }
+        
+
+         public override string ToString()
+         {
+            int i;
+            string ret = "";
+            for (i = 0; i < n; i++)
+                ret += digits[i].ToString();
+            return ret;
+
+         }
+
+        public bool Equals(Number num)// hav to remove
+        {
+            int count = 0, i;
+            if ((object)num == null)
+            {
+                return false;
+            }
+
+            for (i = 0; i < num.n; i++)
+            {
+                if (num.digits[i] == digits[i])//checks of all the digits of the binary num are equals
+                    count++; ;
+            }
+            if (count == n)
+                return true;
+            else
+                return false;
+        }
+
+
+        public static Number operator +(Number b1, Number b2)//adding two binary numbers
+        {
+            int i, j, k;
+            Number num = new Number();
+            bool carry = false;
+            if (b1.n > b2.n)
+                num.n = b1.n;
+            else
+                num.n = b2.n;
+            for (i = b1.n, j = b2.n, k = num.n; i >= 0; i--, j--, k--)
+            {
+                char x = b1.digits[i];
+                char y = j >= 0 ? b2.digits[j] : '0';
+
+                if (carry)
+                {
+                    num.digits[k] = x == y ? '1' : '0';
+                    carry = x == '1' || y == '1';
+                }
+                else
+                {
+                    num.digits[k] = x == y ? '0' : '1';
+                    carry = x == '1' && y == '1';
+                }
+            }
+
+            if (carry)
+            {
+                num.digits[0] = '1';
+               
+            }
+
+            return num;
+
+        }
+        public  Number operator *(Number b1,Number b2)
+        {
+
+            
+            
+                   
+
+            
+
+        }
+        public char [] shiftLeft(char[]arr,int n)
+        {
+            int i;
+            char [] array = new char[arr.Length];
+            array = null;
+            for (i = arr.Length; i > n; i--)
+                array[i-n] = arr[i];
+
+            for (i = 0; i < n; i++)
+                array[arr.Length - i] = '0';
+            return array;
+        }
+    }
+
+}
+
+class Program
     {
         static void Main(string[] args)
         {
+            Number n1, n2, n3;
+            int i;
+
+            n1 = new Number();
+            n2 = new Number();
+            n1.HexString = "F4215";
+            n2.HexString = "A352B";
+            n3 = n1 + n2;
+
+            Console.WriteLine("n1.N = " + n1.N);
+
+            Console.WriteLine("n2.N = " + n2.N);
+
+            Console.WriteLine("n3.N = " + n3.N);
+
+
+            Console.WriteLine("{0} + {1} = {2}",
+                     n1.ToString(), n2.ToString(), n3.ToString());
+
+            Console.WriteLine("{0} + {1} = {2}",
+                     n1.HexString, n2.HexString, n3.HexString);
+
+            Console.WriteLine("digits n1:");
+            for (i = 0; i < n1.N; i++)
+                Console.Write(" {0} ", n1[i]);
+            Console.WriteLine();
+            n3 = n1 * n2;
+
+            Console.WriteLine("n1.N = " + n1.N);
+
+            Console.WriteLine("n2.N = " + n2.N);
+
+            Console.WriteLine("n3.N = " + n3.N);
+
+            Console.WriteLine("{0} * {1} = {2}",
+                     n1.ToString(), n2.ToString(), n3.ToString());
+
+            Console.WriteLine("{0} * {1} = {2}",
+                     n1.HexString, n2.HexString, n3.HexString);
+
+            Console.WriteLine("n1.N = " + n1.N);
+
+            n1 = "1011110";
+            n2 = "1100011";
+            n3 = n1 + n2;
+
+            Console.WriteLine("n1.N = " + n1.N);
+
+            Console.WriteLine("n2.N = " + n2.N);
+
+            Console.WriteLine("n3.N = " + n3.N);
+
+            Console.WriteLine("{0} + {1} = {2}",
+                     n1.ToString(), n2.ToString(), n3.ToString());
+
+            Console.WriteLine("{0} + {1} = {2}",
+                     n1.HexString, n2.HexString, n3.HexString);
+
+            n3 = n1 * n2;
+
+            Console.WriteLine("n1.N = " + n1.N);
+
+            Console.WriteLine("n2.N = " + n2.N);
+
+            Console.WriteLine("n3.N = " + n3.N);
+
+            Console.WriteLine("{0} * {1} = {2}",
+                     n1.ToString(), n2.ToString(), n3.ToString());
+
+            Console.WriteLine("{0} * {1} = {2}",
+                     n1.HexString, n2.HexString, n3.HexString);
+
+            Console.WriteLine("n1.N = " + n1.N);
+
+
+
+
+
+            n1 = new Number("1100011010101110011111", 128);
+
+
+            n2 = new Number("1011101001111000001111", 128);
+
+            Console.WriteLine("n1.N = " + n1.N);
+
+            Console.WriteLine("n2.N = " + n2.N);
+
+            Console.WriteLine("n3.N = " + n3.N);
+
+            n3 = n1 + n2;
+
+            Console.WriteLine("{0} + {1} = {2}",
+                     n1.ToString(), n2.ToString(), n3.ToString());
+
+            Console.WriteLine("{0} + {1} = {2}",
+                     n1.HexString, n2.HexString, n3.HexString);
+
+            n3 = n1 * n2;
+            Console.WriteLine("n1.N = " + n1.N);
+
+            Console.WriteLine("n2.N = " + n2.N);
+
+            Console.WriteLine("n3.N = " + n3.N);
+
+            Console.WriteLine("{0} * {1} = {2}",
+                     n1.ToString(), n2.ToString(), n3.ToString());
+
+            Console.WriteLine("{0} * {1} = {2}",
+                     n1.HexString, n2.HexString, n3.HexString);
+
+
+
+
+            n1 = new Number("11000110101011100111110001111111001111001111100111100110011", 128);
+
+            n2 = new Number("10111010011110000011111111111100000111000111000011100011111", 128);
+
+            n3 = n1 + n2;
+
+            Console.WriteLine("n1.N = " + n1.N);
+
+            Console.WriteLine("n2.N = " + n2.N);
+
+            Console.WriteLine("n3.N = " + n3.N);
+
+            Console.WriteLine("{0} + {1} = {2}",
+                     n1.ToString(), n2.ToString(), n3.ToString());
+
+            Console.WriteLine("{0} + {1} = {2}",
+                     n1.HexString, n2.HexString, n3.HexString);
+
+            n3 = n1 * n2;
+
+            Console.WriteLine("n1.N = " + n1.N);
+
+            Console.WriteLine("n2.N = " + n2.N);
+
+            Console.WriteLine("n3.N = " + n3.N);
+
+            Console.WriteLine("{0} * {1} = {2}",
+                     n1.ToString(), n2.ToString(), n3.ToString());
+
+            Console.WriteLine("{0} * {1} = {2}",
+                     n1.HexString, n2.HexString, n3.HexString);
+
+
+
         }
     }
 }
---
+++ labs-realTime/test2.asm
@@ -0,0 +1,174 @@
+;
+; Cursor2.asm
+;
+;This program demonstrates cursor manipulation.
+;
+;
+Stak              SEGMENT PARA STACK 'STACK'
+DB                 256 DUP(0)
+Stak              ENDS
+;
+Data               SEGMENT PARA PUBLIC 'Data'
+CursorPos          DW      0     ; Number of lines scrolled down
+Base               DW      0
+var                DB      0Eh      
+        
+;
+Data               ENDS
+Code               SEGMENT PARA PUBLIC 'Code'
+                   .386  ; Enable 386 commands
+SetCursorPos          PROC    NEAR
+; Set Cursor to position in BX
+; Input: BX
+; Output: None
+;
+  MOV              DX,3D4H  ; Point to 3D4H - 3D5H port pair
+  MOV              AL,14    ; Address of cursor register pos high byte
+  MOV              AH,BH    ; Get desired value of cursor pos high byte
+  OUT              DX,AX    ; Port(3D4h) = 14, Port(3D5h) = Value of BH
+;
+  MOV              AL,15    ; Address of cursor register pos low byte
+  MOV              AH,BL    ; Get desired value pf cursor pos low byte
+  OUT              DX,AX    ; Port(3D4h) = 15, Port(3D5h) = Value of BL
+;
+  RET                       ; Return to caller
+SetCursorPos          ENDP
+;
+Start              PROC    FAR
+;
+;STANDARD PROGRAM PROLOGUE
+;
+  ASSUME           CS:Code
+  PUSH             DS          ; Save PSP segment address
+  MOV              AX,0
+  PUSH             AX          ; Save INT 20h address offset (PSP+0)
+  MOV              AX,Data
+  MOV              DS,AX       ; Establish Data segment addressability
+  ASSUME           DS:Data
+;
+;Part1 : Initialize the display adapter
+;
+  MOV              AH,0          ; Select function = 'Set mode'
+  MOV              AL,1          ; 40 by 25 color image
+  INT              10h           ; Adapter initialized. Page 0 displayed
+;
+  MOV              AX,0B800h     ; Segment address of memory on color adapter
+;
+  MOV              ES,AX         ; Set up extra segment register
+  MOV              DI,0          ; Initial offset address into segment
+  MOV              AL,' '        ; Character space to fill adapter memory
+  MOV              AH,0Eh        ; Attribute byte : Intense yellow
+  MOV              CX,1000       ; Initialize count, 1 Screen
+  CLD                            ; Write forward
+  REP              STOSW         ; Write
+;
+; Write 'A' in mid screen
+;
+  MOV              BYTE PTR ES:[2*(12*40+20)],'A'
+;
+; Set the cursor address registers
+;
+  MOV              BX,12*40+20
+  CALL             SetCursorPos
+;
+;PART 2 : Wait for key strike
+;
+; Wait for key
+;
+NextLoop:
+  MOV              AH,0       ; Wait and read key
+  INT              16h        ;
+  CMP              AH,1       ; Is it Esc?
+  JE               ToReturn   ; Yes - Return to DOS
+;
+;  Not esc key - change cursor
+    CMP AH,75
+    JE LEFT
+    CMP AH,77
+    JE RIGHT
+    CMP AH,72
+    JE UP
+    CMP AH,80
+    JE DOWN
+    CMP AH,60
+    JE MAXCURSOR
+    CMP AH,61
+    JE MINCURSOR
+    CMP AH,59
+    JE ATTRIBUTE
+    JMP LETTER
+  LEFT:
+       CMP DI,0
+       jng LABEL
+       DEC DI
+       MOV BX,DI
+       JMP LABEL
+   RIGHT:
+       CMP DI,999
+       JGE LABEL
+       INC DI
+       MOV BX,DI
+       JMP LABEL
+   UP:
+       CMP DI,39
+       JNG LABEL
+       SUB DI,40
+       MOV BX,DI
+       JMP LABEL
+   DOWN:
+       CMP DI,960
+       JGE LABEL
+       ADD DI,40
+       MOV BX,DI
+       JMP LABEL
+ ; 3D4H  Graphics adapter address register port
+; 3D5H  Graphics adapter data register port
+;
+   MAXCURSOR:
+       MOV DX,3D4h
+       MOV AX,0E0Ah  ; Cursor start address (0Ah) - Value 14 (0Dh)
+       OUT DX,AX     ; Port(3D4h) = 0Ah, Port(3D5h) = 01h
+       MOV AX,0F0Bh  ; Cursor end address - Value 15 (0Eh)
+       OUT DX,AX     ; Port(3D4h) = 0Bh, Port(3D5h) = 0Eh
+       JMP  LABEL
+    MINCURSOR:   
+       MOV  DX,3D4h  ; Point TO 3D4h - 3D5h port pair
+       MOV  AX,000Ah ; Cursor start address (0Ah) - Value 0 (00h)
+       OUT  DX,AX    ; Port(3D4h) = 0Ah, Port(3D5h) = 01h
+       MOV  AX,0F0Bh ; Cursor end address - Value 15 (0Eh)
+       OUT   DX,AX    ; Port(3D4h) = 0Bh, Port(3D5h) = 0Eh
+       JMP LABEL
+    ATTRIBUTE:
+       MOV  Dl,var 
+       INC  Dl
+       MOV  var,Dl
+
+       MOV   AH,var
+       SHL   DI,1
+       MOV   AL,BYTE PTR ES:[DI]
+       MOV   ES:[DI],AX
+       SHR   DI,1
+       JMP  LABEL
+    
+    
+   
+    LETTER:
+       MOV BX,DI
+       SHL DI,1 ; set the cursor in  not even offset position  
+       MOV BYTE PTR ES:[DI],AL ;read the letter
+       SHR DI,1 ; set rhe position of the cursor after we read the letter
+       CMP DI,998
+       JG LABEL
+       ADD DI,1
+    LABEL:
+       MOV BX,DI
+       CALL setCursorPos
+       JMP  NextLoop   ; Repeat main loop
+;
+ToReturn:
+                   MOV  AX,2
+                   INT  10h
+                   RET
+Start              ENDP
+Code               ENDS
+  END              Start
